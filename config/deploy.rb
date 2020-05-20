2f1f01d423a5302f423e9b5b5add61e64880b83b
takapi <happyturn86b@gmail.com>
2020-05-21 02:38:13 +0900

    デプロイ時にウィルススキャンをする

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,3 +5,14 @@ set :repo_url, "https://github.com/takapi86/clamdscan-on-capistrano.git"
 set :deploy_to, '/var/www/app'
 set :user, 'www-data'
 set :group, 'www-data'
+
+namespace :clamd do
+  desc 'clamdscan'
+  task :scan do
+    on roles(:all) do
+      execute :clamdscan, "--remove", release_path
+    end
+  end
+end
+
+before 'deploy:publishing', 'clamd:scan'
